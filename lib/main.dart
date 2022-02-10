da8183923149e830889c1559daf5b959f69f157b
MacPro13 <macpro13@kit2512.local>
2022-02-10 11:35:09 +0700

    finish login and sign up screen/auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:bhsoft_instagram_clone/firebase_options.dart';
-import 'package:bhsoft_instagram_clone/responsive/responsive.dart';
+import 'package:bhsoft_instagram_clone/ui/screens/screens.dart';
 import 'package:bhsoft_instagram_clone/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,20 +7,19 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(InstagramApp());
 }
 
-class MyApp extends StatelessWidget {
+class InstagramApp extends StatelessWidget {
+  const InstagramApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Instagram Clone",
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: kmobileBackgroundColor),
-      home: ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      home: LoginScreen(),
     );
   }
 }
