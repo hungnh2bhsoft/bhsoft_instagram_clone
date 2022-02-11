7c514cdcaee261c9312df482a43976a19637e13b
MacPro13 <macpro13@kit2512.local>
2022-02-11 14:46:55 +0700

    finish adding bottom appbar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:bhsoft_instagram_clone/models/models.dart';
 import 'package:bhsoft_instagram_clone/providers/user_provider.dart';
+import 'package:bhsoft_instagram_clone/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,13 +15,68 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _currentIndex = 0;
+  final PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).user!;
     return Scaffold(
-      body: Center(
-        child: Text(user.username),
+      body: PageView(
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: [
+          Center(
+            child: Text("Home page"),
+          ),
+          Center(
+            child: Text("Search page"),
+          ),
+          Center(
+            child: Text("Add page"),
+          ),
+          Center(
+            child: Text("Favorite page"),
+          ),
+        ],
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        activeColor: kprimaryColor,
+        inactiveColor: kSecondaryColor,
+        currentIndex: _currentIndex,
+        onTap: selectPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "",
+            backgroundColor: kprimaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "",
+            backgroundColor: kprimaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: "",
+            backgroundColor: kprimaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "",
+            backgroundColor: kprimaryColor,
+          ),
+        ],
       ),
     );
+  }
+
+  void selectPage(index) {
+    _pageController.jumpToPage(index);
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
