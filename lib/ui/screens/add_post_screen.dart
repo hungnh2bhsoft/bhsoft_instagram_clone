b1edd7bd3327600bad24426256fbca38a36231bb
= <=>
2022-02-14 10:18:44 +0700

    finish

--- lib/ui/screens/add_post_screen.dart
+++ lib/ui/screens/add_post_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:bhsoft_instagram_clone/providers/user_provider.dart';
+import 'package:bhsoft_instagram_clone/resources/firestore_methods.dart';
 import 'package:bhsoft_instagram_clone/utils/colors.dart';
 import 'package:bhsoft_instagram_clone/utils/utils.dart';
 import 'package:flutter/material.dart';
@@ -15,17 +16,51 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  final _descriptionController = TextEditingController();
   Uint8List? _file;
+
+  void _clearPost() {
+    setState(() {
+      _file = null;
+    });
+  }
+
+  void _postImage(String uid, String username, String profImage) async {
+    try {
+      await FirestoreMethods().uploadPost(
+        _descriptionController.text,
+        _file!,
+        uid,
+        username,
+        profImage,
+      );
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(
+          const SnackBar(
+            content: Text('Post added successfully'),
+          ),
+        );
+      _clearPost();
+    } on UploadPostFailure catch (e) {
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(
+          SnackBar(content: Text(e.message)),
+        );
+    }
+  }
+
   void _selectImage() async {
     return showDialog(
       context: context,
       builder: (context) {
         return SimpleDialog(
-          title: Text("Choose Image"),
+          title: const Text("Choose Image"),
           children: [
             SimpleDialogOption(
-              padding: EdgeInsets.all(20.0),
-              child: Text("Take a photo"),
+              padding: const EdgeInsets.all(20.0),
+              child: const Text("Take a photo"),
               onPressed: () async {
                 Navigator.of(context).pop();
                 final file = await pickImage(ImageSource.camera);
@@ -35,8 +70,8 @@ class _AddPostScreenState extends State<AddPostScreen> {
               },
             ),
             SimpleDialogOption(
-              padding: EdgeInsets.all(20.0),
-              child: Text("Choose from gallery"),
+              padding: const EdgeInsets.all(20.0),
+              child: const Text("Choose from gallery"),
               onPressed: () async {
                 Navigator.of(context).pop();
                 final file = await pickImage(ImageSource.camera);
@@ -44,6 +79,11 @@ class _AddPostScreenState extends State<AddPostScreen> {
                   _file = file;
                 });
               },
+            ),
+            SimpleDialogOption(
+              padding: const EdgeInsets.all(20.0),
+              child: const Text("Cancel"),
+              onPressed: () => Navigator.of(context).pop(),
             ),
           ],
         );
@@ -56,15 +96,31 @@ class _AddPostScreenState extends State<AddPostScreen> {
     final user = Provider.of<UserProvider>(context, listen: false).user;
     return _file == null
         ? Center(
-            child: IconButton(
-              icon: Icon(Icons.upload),
-              onPressed: _selectImage,
+            child: GestureDetector(
+              onTap: _selectImage,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.upload),
+                  Text(
+                    "Select an image",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         : Scaffold(
             appBar: AppBar(
               backgroundColor: kMobileBackgroundColor,
               title: const Text("Post to"),
+              leading: IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: _clearPost,
+              ),
               actions: [
                 TextButton(
                   child: const Text(
@@ -75,7 +131,11 @@ class _AddPostScreenState extends State<AddPostScreen> {
                     ),
                   ),
                   onPressed: () {
-                    // TODO: Handle posting
+                    _postImage(
+                      user!.uid,
+                      user.username,
+                      user.imageUrl,
+                    );
                   },
                 )
               ],
@@ -94,6 +154,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
                     SizedBox(
                       width: MediaQuery.of(context).size.width * 0.3,
                       child: TextField(
+                        controller: _descriptionController,
                         decoration: const InputDecoration(
                             hintText: "Write a caption...",
                             border: InputBorder.none),
